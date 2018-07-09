9328bc81e9f6935249350c5fa937f4cd072ea45c
ittisafur <ittisafur@gmail.com>
2018-07-10 02:05:15 +0600

    Adding Controllers

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('user.blog');
+
+// User Routes
+Route::group(['namespace' => 'User'], function(){
+	Route::get('/', 'HomeController@index');
+	Route::get('/post', 'PostController@index')->name('post');	
 });
 
-Route::get('/post', function () {
-    return view('user.post');
-})->name('post');
-
-Route::get('admin/home', function () {
-    return view('admin.home');
-});
-
-Route::get('admin/post', function () {
-    return view('admin.post.post');
-});
-
-Route::get('admin/tag', function () {
-    return view('admin.tag.tag');
+// Admin Routes
+Route::group(['namespace' => 'Admin'], function() {
+	Route::get('admin/home', 'HomeController@index')->name('admin.home');
+    Route::resource('admin/user', 'UserController');
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::resource('admin/category', 'CategoryController');
 });
 
 
-Route::get('admin/category', function () {
-    return view('admin.category.category');
-});
